f56b5e8a59e4e6540e080d4c12ad164d9f08f5e7
robertlabrie <robert.labrie@gmail.com>
2013-08-31 15:28:21 -0400

    more changes to the test rig

--- examples/test.php
+++ examples/test.php
@@ -1,4 +1,15 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
+require_once("./Browscap.php");
 
-$can = new CanIUse\CanIUse();
+use phpbrowscap\Browscap;
+$bc = new Browscap('/tmp');
+$browser = $bc->getBrowser();
+echo "<textarea rows=25 cols=80>" . var_export($browser,true) . "</textarea>";
+
+$can = new phpcaniuse\CanIUse($browser,file_get_contents("../data.json"));
+echo "<br><pre>" . var_export($can->getInfo(),true) . "</pre><br>";
+echo "<br><textarea rows=25 cols=80>" . var_export($can->browserCan(),true) . "</textarea><br>";
+echo "<br><textarea rows=25 cols=80>" . var_export($can->featureList(),true) . "</textarea><br>";
+echo "<textarea rows=25 cols=80>" . var_export($can->data,true) . "</textarea>";
+echo "\n<br>fin<br>\n";
