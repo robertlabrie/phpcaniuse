dca592c007889699a490211bb56553c3260033f9
robertlabrie <robert.labrie@gmail.com>
2013-09-01 05:03:36 -0400

    add stuff

--- examples/test.php
+++ examples/test.php
@@ -1,8 +1,7 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
-
-
 use phpbrowscap\Browscap;
+use phpcaniuse\CanIUse
 $bc = new Browscap('/tmp');
 $browser = $bc->getBrowser();
 $can = new phpcaniuse\CanIUse($browser,file_get_contents("../data.json"));
