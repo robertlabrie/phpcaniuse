22cf6cec774d6e6b990bb56105595b11cca5d0ca
robertlabrie <robert.labrie@gmail.com>
2013-09-01 03:49:34 -0400

    more dev/tst/dev

--- examples/test.php
+++ examples/test.php
@@ -5,11 +5,13 @@ require_once("./Browscap.php");
 use phpbrowscap\Browscap;
 $bc = new Browscap('/tmp');
 $browser = $bc->getBrowser();
-echo "<textarea rows=25 cols=80>" . var_export($browser,true) . "</textarea>";
-
 $can = new phpcaniuse\CanIUse($browser,file_get_contents("../data.json"));
-echo "<br><pre>" . var_export($can->getInfo(),true) . "</pre><br>";
-echo "<br><textarea rows=25 cols=80>" . var_export($can->browserCan(),true) . "</textarea><br>";
-echo "<br><textarea rows=25 cols=80>" . var_export($can->featureList(),true) . "</textarea><br>";
-echo "<textarea rows=25 cols=80>" . var_export($can->data,true) . "</textarea>";
+
+echo "supported:" . $can->check(array('fontface','video')) . "<br>";
+echo "browser<br><textarea rows=25 cols=80>" . var_export($browser,true) . "</textarea>";
+
+echo "<br>getInfo<br><textarea rows=10 cols=80>" . var_export($can->getInfo(),true) . "</textarea><br>";
+echo "<br>browserCan<br><textarea rows=25 cols=80>" . var_export($can->browserCan(),true) . "</textarea><br>";
+echo "<br>featureList<br><textarea rows=25 cols=80>" . var_export($can->featureList(),true) . "</textarea><br>";
+echo "<br>raw data<br><textarea rows=25 cols=80>" . var_export($can->data,true) . "</textarea>";
 echo "\n<br>fin<br>\n";
